cccb8565eaeee00b05a49f470e0d366e4f22cb52
Shravani Lokhande <149851376+lokhandesk7278@users.noreply.github.com>
2026-03-17 18:54:16 +0530

    Added Two Sum two-pointer solution

---
+++ Two Pointers/Pair_Target.java
@@ -0,0 +1,35 @@
+class Pair_Target {
+
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left < right) {
+            int currentSum = numbers[left] + numbers[right];
+
+            if (currentSum == target) {
+                return new int[]{left + 1, right + 1};
+            } 
+            else if (currentSum < target) {
+                left++;
+            } 
+            else {
+                right--;
+            }
+        }
+
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+
+        Pair_Target obj = new Pair_Target();
+
+        int numbers[] = {2,7,11,15};
+        int target = 9;
+
+        int result[] = obj.twoSum(numbers, target);
+
+        System.out.println(result[0] + " " + result[1]);
+    }
+}
